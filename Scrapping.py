d615b9475d81c7ffcdb2b0f6e83b354708e24c11
Tmegaa <68335587+Tmegaa@users.noreply.github.com>
2024-11-16 15:19:40 +0100

    Code de base pour extraire des données

---
+++ Scrapping.py
@@ -0,0 +1,78 @@
+from bs4 import BeautifulSoup
+import requests
+import csv
+import re  # module for regular expression
+
+if __name__ == "__main__":
+
+    links = []  # ensemble des sites web interessants
+    response = requests.get(
+        'https://www.jedha.co/formation-cybersecurite/liste-des-organisations-victimes-dune-cyberattaque-en-2024')
+    soup = BeautifulSoup(response.content, "html.parser")
+    soup.prettify()
+    print(response)  # looking if the connection worked
+
+
+    def get_all_line():
+        all_line = []
+        count=0
+        for item in soup.select("li"):
+            text = item.get_text(strip=True)
+            if count>1:
+                all_line.append(text)
+            count+=1
+        return all_line
+
+
+    def get_entreprise():
+        entreprise = []
+        for item in soup.select("li strong"):
+            text = item.get_text(strip=True)
+            entreprise.append(text)
+        return entreprise
+
+
+    def get_dates():
+        dates = []
+        for item in soup.select("li"):
+            text = item.get_text()
+            date_match = re.search(r'\d{1,2} \w+ 2024', text)
+            if date_match:
+                dates.append(date_match.group())
+        return dates
+
+
+    def get_type():
+        type = []
+
+        for item in soup.select("li"):
+            text = item.get_text()
+            type_match = re.search(r'-\s+\d{1,2} \w+ 2024\s+-\s+(.*?)\s*\(', text)
+            if type_match:
+                type.append(type_match.group(1).strip())
+        return type
+
+
+    print(len(get_entreprise()))
+    print(get_dates())
+    print(len(get_type()))
+    print(len(get_all_line()))
+
+    for elt in get_entreprise():
+        print(elt)
+    #print (len(get_all_line()))
+
+
+# insert into the csv file
+"""
+with open('cyber_attack.csv', 'w', newline='', encoding='utf-8') as file:
+    writer = csv.writer(file)
+    field = ["entreprise","date","type"]
+    ls_entreprise = get_entreprise()
+    ls_dates = get_dates()
+    ls_types = get_type()
+    writer.writerow(field)
+    for i in range(len(get_entreprise())):
+        writer.writerow([ls_entreprise[i],ls_dates[i],ls_types[i]])
+    file.close()
+"""
