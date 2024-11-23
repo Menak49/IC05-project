e6e4ecb486df93fe36bda3bfbe44ca42f7a0d436
Menak49 <146824802+Menak49@users.noreply.github.com>
2024-11-23 18:24:17 +0100

    amélioration de l'affichage

--- Affichage_data.py
+++ Affichage_data.py
@@ -1,7 +1,4 @@
 
-
-
-
 import pandas as pd
 
 # Modifier les options d'affichage
@@ -9,18 +6,27 @@ pd.set_option('display.max_columns', None)  # Affiche toutes les colonnes
 pd.set_option('display.width', None)       # Ajuste la largeur pour éviter les coupures
 pd.set_option('display.max_colwidth', None)  # Affiche tout le contenu des cellules
 
-
-fichier_excel = "Data2.xlsx" # Modifier le nom pour changer de fichier
-
+fichiers_excel = ["Data2.xlsx", "Data1.xlsx", "cyber_attack.csv"]# Liste des fichiers à traiter à modifier selon les besoins
 
 
-df = pd.read_excel(fichier_excel)
+for fichier in fichiers_excel:
+    if fichier.endswith('.xlsx'):  #Si le fichier est un Excel
+        df = pd.read_excel(fichier)
+    elif fichier.endswith('.csv'):  #Si le fichier est un CSV
+        df = pd.read_csv(fichier)
+    else:
+        print(f"Erreur pour : {fichier}")
+
+
+    print(f"\nFichier : {fichier}")
+    print(df.iloc[0:10])
 
 
 
 
-# Afficher les premières lignes
-print(df.iloc[0:10])
+
+
+
 
 
 
