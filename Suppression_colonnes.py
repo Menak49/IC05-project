c9647643baab7715e6235ca81a986fc6b64b77bc
Menak49 <146824802+Menak49@users.noreply.github.com>
2024-11-23 18:47:07 +0100

    amélioration de la suppression

--- Suppression_colonnes.py
+++ Suppression_colonnes.py
@@ -10,24 +10,32 @@ pd.set_option('display.width', None)       # Ajuste la largeur pour éviter les 
 pd.set_option('display.max_colwidth', None)  # Affiche tout le contenu des cellules
 
 
-fichier_excel = "Data2.xlsx"
+fichier_excel = "Data1.xlsx"
+nb = fichier_excel.find('.')
 
 
 
-# Charger la première feuille du fichier Excel
+
 df = pd.read_excel(fichier_excel)
 
 print(df.iloc[0:5])  # Affichage avant
 
+
+colonne_limite = 'cyber_conflict_issue'
+
+
+
 # Supprimer les colonnes (par nom)
-colonnes_a_supprimer = ['month', 'industry_code', 'event_subtype', 'source_url']
+colonnes_a_supprimer = ['inclusion_criteria_subcode', 'source_incident_detection_disclosure', 'receiver_region', 'receiver_category','initiator_category','number_of_attributions','attribution_ID','attribution_date','attribution_type','attribution_basis', 'attributing_actor',
+                        'attribution_it_company', 'attributing_country','attributed_initiator', 'attributed_initiator_country', 'attributed_initiator_category' ]#changer les colonnes en fonction des besoins
 df = df.drop(columns=colonnes_a_supprimer)
+
 
 
 print("\n\nFichier nettoyé de ses colonnes : \n")
 
-# Sauvegarder dans un nouveau fichier Excel
-df.to_excel("Data2_v2.xlsx", index=False)
+# sauvegarder dans un nouveau fichier Excel
+df.to_excel(fichier_excel[nb] + "_v2" + ".xlsx", index=False)
 
 print(df.iloc[0:10])  # Affichage apres
 
